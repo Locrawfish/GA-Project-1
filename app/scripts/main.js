a1198f8fa872dc25a003a3f2a5dfedec57f918a9
Loren Crawford <lmcrawf@gmail.com>
2015-08-25 12:05:25 -0400

    Got jQuery to tell user if answer selection was correct or not

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,57 +1,74 @@
   'use strict';
 
-console.log('Welcome to Globetrotter!');
+  console.log('Welcome to Globetrotter!');
 
-// Hide quiz section until click the top button
-$('#quiz').hide();
+  // Hide quiz section until click the top button
+  $('#quiz').hide();
 
-// Array of questions and answers
-// function countryQandAs(country, question, answer){
-//   this.country = country;
-//   this.question = question;
-//   this.answer = answer;
-// };
+  // Array of questions and answers
+  // function countryQandAs(country, question, answer){
+  //   this.country = country;
+  //   this.question = question;
+  //   this.answer = answer;
+  // };
 
- var questions = [{
-      country: 'Austria',
-      question: 'What is the capital of Austria?',
-      choices: ['Salzburg', 'Vienna', 'Linz'],
-      correctAnswer: 1,
-      image: '<img src=\'images/Austria.jpg\'>'
-    },{
-      country: 'Germany',
-      question: 'When did the Berlin Wall fall?',
-      choices: ['1990','1989','1987'],
-      correctAnswer: 1,
-      image: '<img src=\'images/Germany.jpg\'>'
-    },{
-      country: 'United Kingdom',
-      question: "Using the picture to the right, what does 'Big Ben' refer to?",
-      choices: ['The tower', 'The clock', 'The bell of the clock'],
-      correctAnswer: 2,
-      image: '<img src=\'images/United_Kingdom.jpg\'>'
-    }];
+  var questions = [{
+    country: 'Austria',
+    question: 'What is the capital of Austria?',
+    choices: ['Salzburg', 'Vienna', 'Linz'],
+    correctAnswer: 1,
+    image: '<img src=\'images/Austria.jpg\'>'
+  }, {
+    country: 'Germany',
+    question: 'When did the Berlin Wall fall?',
+    choices: ['1990', '1989', '1987'],
+    correctAnswer: 1,
+    image: '<img src=\'images/Germany.jpg\'>'
+  }, {
+    country: 'United Kingdom',
+    question: "Using the picture to the right, what does 'Big Ben' refer to?",
+    choices: ['The tower', 'The clock', 'The bell of the clock'],
+    correctAnswer: 2,
+    image: '<img src=\'images/United_Kingdom.jpg\'>'
+  }];
 
-    var quiz = $('#quiz');
-    var score = 0;
+  var quiz = $('#quiz');
+  var score = 0;
+  var selection;
+  var randomNumber = Math.round(Math.random() * 2); //need to change the 2 to the number of questions -1
+  var randomQuestion = questions[randomNumber];
 
-// Show quiz section until click the top button
-$('#main-button').click(function(){
+  // Show quiz section until click the top button
+  $('#main-button').click(function() {
   $('#travel').hide(800);
-  $('#quiz').show(1000, function(New){
-    var randomNumber = Math.round(Math.random()*2);
-    var randomQuestion = questions[randomNumber];
+  $('#quiz').show(1000, function(New) {
+    // var randomNumber = Math.round(Math.random()*2); //need to change the 2 to the number of questions -1
+    // var randomQuestion = questions[randomNumber];
     $('#country-name').text(randomQuestion.country);
     $('img').replaceWith(randomQuestion.image);
     $('#country-question').text(randomQuestion.question);
     $('#answer-one').text(randomQuestion.choices[0]);
     $('#answer-two').text(randomQuestion.choices[1]);
     $('#answer-three').text(randomQuestion.choices[2]);
+
+    // $('#submit-button').click(function() {
+    //     if (selection[i]) === randomQuestion.correctAnswer)
+    // }
+  // });
   });
+  });
+
+$('#myForm input').on('change', function() {
+   selection = ($('input[name="optradio"]:checked', '#myForm').val());
+   // console.log('answer is ' + randomQuestion.correctAnswer);
+   if (selection == randomQuestion.correctAnswer){
+    alert("You're correct!");
+   } else{
+    alert("You're wrong. Try again.");
+   }
 });
 
 
-// if $('#correctAnswer')
-
-
-
+  // if $('#correctAnswer')
+  // if ($('.radio') == randomQuestion.correctAnswer){
+  // $('#quiz').show(1000, function(New));
